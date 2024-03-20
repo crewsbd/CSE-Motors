0b5008df1d72b20c99686c6c6a3392120a268803
crewsbd <bdcrews@protonmail.com>
2024-03-20 01:46:19 -0700

    Server-side Data Validation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -22,6 +22,7 @@ async function buildRegister(req, res, next) {
     res.render("account/register", {
       title: "Register",
       nav,
+      errors: null,
     })
   }
   
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,12 +1,18 @@
-// Needed Resources 
+// Needed Resources
 const express = require("express");
-const router = new express.Router() ;
+const router = new express.Router();
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build account view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/registration", utilities.handleErrors(accountController.buildRegister));
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
